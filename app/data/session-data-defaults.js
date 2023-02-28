bab8cceecd05c8af0ec159d5a518c1ba1609ff41
James Medd <james.medd@sparck.io>
2023-02-28 16:08:54 +0000

    Supplier pages (#7)
    * start on supplier entry
    
    * Add supplier and meter functionality

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -1,5 +1,91 @@
 module.exports = {
-
-  // Insert values here
-
+  "suppliers": [
+    {
+      "supplierName": "BlueFlame Energy"
+    },
+    {
+      "supplierName": "BlueSky Power"
+    },
+    {
+      "supplierName": "BrightSpark Energy"
+    },
+    {
+      "supplierName": "CleanCurrents UK"
+    },
+    {
+      "supplierName": "CleanGenius UK"
+    },
+    {
+      "supplierName": "ClimateWise Energy"
+    },
+    {
+      "supplierName": "EcoEnergi UK"
+    },
+    {
+      "supplierName": "EcoWave Energy"
+    },
+    {
+      "supplierName": "EverGreen Energy"
+    },
+    {
+      "supplierName": "GreenGen UK"
+    },
+    {
+      "supplierName": "GreenGlow Energy"
+    },
+    {
+      "supplierName": "GreenLife Energy"
+    },
+    {
+      "supplierName": "NaturePower UK"
+    },
+    {
+      "supplierName": "OceanBreeze Energy"
+    },
+    {
+      "supplierName": "PowerPlus UK"
+    },
+    {
+      "supplierName": "PureEnergy Solutions"
+    },
+    {
+      "supplierName": "PureWatt Energy"
+    },
+    {
+      "supplierName": "RedSky Energy"
+    },
+    {
+      "supplierName": "RenewGreen UK"
+    },
+    {
+      "supplierName": "Skyline Power"
+    },
+    {
+      "supplierName": "SolarCoast Energy"
+    },
+    {
+      "supplierName": "Solaris Energy"
+    },
+    {
+      "supplierName": "SunScape Energy"
+    },
+    {
+      "supplierName": "SunStream Energy"
+    },
+    {
+      "supplierName": "Sustainable Power Co."
+    },
+    {
+      "supplierName": "TerraWatt Energy"
+    },
+    {
+      "supplierName": "Windward Energy"
+    },
+    {
+      "supplierName": "WindWise Energy"
+    },
+    {
+      "supplierName": "WiseWatt Energy"
+    }
+  ]
 }
