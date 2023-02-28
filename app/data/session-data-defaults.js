bf780bc404c08fd366de1ca259f9fa32063f6360
James Medd <james.medd@sparck.io>
2023-02-28 11:08:59 +0000

    start on supplier entry

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -1,5 +1,70 @@
 module.exports = {
-
-  // Insert values here
-
+  "suppliers": [
+    {
+      "supplierName": "Supplier 1"
+    },
+    {
+      "supplierName": "Supplier 2"
+    },
+    {
+      "supplierName": "Supplier 3"
+    },
+    {
+      "supplierName": "Supplier 4"
+    },
+    {
+      "supplierName": "Supplier 5"
+    },
+    {
+      "supplierName": "Supplier 6"
+    },
+    {
+      "supplierName": "Supplier 7"
+    },
+    {
+      "supplierName": "Supplier 8"
+    },
+    {
+      "supplierName": "Supplier 9"
+    },
+    {
+      "supplierName": "Supplier 10"
+    },
+    {
+      "supplierName": "Supplier 11"
+    },
+    {
+      "supplierName": "Supplier 12"
+    },
+    {
+      "supplierName": "Supplier 13"
+    },
+    {
+      "supplierName": "Supplier 14"
+    },
+    {
+      "supplierName": "Supplier 15"
+    },
+    {
+      "supplierName": "Supplier 16"
+    },
+    {
+      "supplierName": "Supplier 17"
+    },
+    {
+      "supplierName": "Supplier 18"
+    },
+    {
+      "supplierName": "Supplier 19"
+    },
+    {
+      "supplierName": "Supplier 20"
+    },
+    {
+      "supplierName": "Supplier 21"
+    },
+    {
+      "supplierName": "Supplier 22"
+    }
+  ]
 }
--- app/routes.js
+++ app/routes.js
@@ -6,4 +6,25 @@
 const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
-// Add your routes here
+router.get(['/start'], (req, res) => {
+  req.session.data = {
+    providers: providers,
+    cookies: req.session.data.cookies
+  }
+  res.render('start.html')
+})
+
+router.get(['/cookie-choice'], (req, res) => {
+  const cookieChoice = req.query.choice === 'accept'
+  req.session.data.cookies = cookieChoice
+  res.redirect(req.headers.referer)
+})
+
+router.get(['/add-gas-supplier'], (req, res) => {
+  if (req.session.data.gasSuppliers === undefined) req.session.data.gasSuppliers = []
+  req.session.data.gasSuppliers.push({
+    provider: req.session.data['gas-supplier'],
+    meterNumbers: []
+  })
+  res.redirect('/gas-provider-summary')
+})
