e770e32094aff9033ccccc2e65e96284c45949c5
James Medd <james.medd@sparck.io>
2023-03-02 14:56:38 +0000

    Start adding HN journey (#18)

--- app/routes.js
+++ app/routes.js
@@ -19,6 +19,11 @@ router.get(['/cookie-choice'], (req, res) => {
   res.redirect(req.headers.referer)
 })
 
+router.get(['/set-path'], (req, res) => {
+  req.session.data.path = req.session.data['etii-or-hn']
+  res.redirect('/connected-to-electricity-gas-both')
+})
+
 router.get(['/connection-check'], (req, res) => {
   switch (req.session.data['connected-to-gas-electricity']) {
     case 'no':
@@ -26,7 +31,23 @@ router.get(['/connection-check'], (req, res) => {
       break
     case 'yes':
     default:
-      res.redirect('/organisation-name')
+      if (req.session.data.path === 'heat-network') {
+        res.redirect('/heat-network-serve-domestic-customer')
+      } else {
+        res.redirect('/organisation-name')
+      }
+      break
+  }
+})
+
+router.get(['/domestic-check'], (req, res) => {
+  switch (req.session.data['hn-serve-domestic']) {
+    case 'no':
+      res.redirect('/must-serve-at-least-one-domestic-customer')
+      break
+    case 'yes':
+    default:
+      res.redirect('/who-can-we-contact')
       break
   }
 })
@@ -78,14 +99,14 @@ router.get(['/energy-check'], (req, res) => {
 router.get(['/add-gas-supplier'], (req, res) => {
   const supplierName = req.session.data['gas-supplier']
   if (req.session.data.gasSuppliers === undefined) req.session.data.gasSuppliers = []
-  req.session.data.gasSuppliers.find(entry => entry.supplier === supplierName) ?? req.session.data.gasSuppliers.push({ supplier: supplierName, meterNumbers: [] })
+  req.session.data.gasSuppliers.find(entry => entry.supplier === supplierName) ?? req.session.data.gasSuppliers.push({ supplier: supplierName, meterNumbers: [], chp: [], supply: [], egc: [], besides: [] })
   res.redirect('/what-is-your-gas-meter-number')
 })
 
 router.get(['/add-electricity-supplier'], (req, res) => {
   const supplierName = req.session.data['electricity-supplier']
   if (req.session.data.electricitySuppliers === undefined) req.session.data.electricitySuppliers = []
-  req.session.data.electricitySuppliers.find(entry => entry.supplier === supplierName) ?? req.session.data.electricitySuppliers.push({ supplier: supplierName, meterNumbers: [] })
+  req.session.data.electricitySuppliers.find(entry => entry.supplier === supplierName) ?? req.session.data.electricitySuppliers.push({ supplier: supplierName, meterNumbers: [], chp: [], supply: [], egc: [], besides: [] })
   res.redirect('/what-is-your-electricity-meter-number')
 })
 
@@ -93,14 +114,85 @@ router.get(['/add-gas-meter'], (req, res) => {
   const supplierName = req.session.data['gas-supplier']
   const supplier = req.session.data.gasSuppliers.find(entry => entry.supplier === supplierName)
   supplier.meterNumbers.push(req.session.data['meter-number'])
-  res.redirect('/gas-supplier-summary')
+  if (req.session.data.path === 'heat-network') {
+    res.redirect('/is-this-a-chp')
+  } else {
+    res.redirect('/gas-supplier-summary')
+  }
 })
 
 router.get(['/add-electricity-meter'], (req, res) => {
   const supplierName = req.session.data['electricity-supplier']
   const supplier = req.session.data.electricitySuppliers.find(entry => entry.supplier === supplierName)
   supplier.meterNumbers.push(req.session.data['meter-number'])
-  res.redirect('/electricity-supplier-summary')
+  if (req.session.data.path === 'heat-network') {
+    res.redirect('/is-this-a-chp')
+  } else {
+    res.redirect('/electricity-supplier-summary')
+  }
+})
+
+router.get(['/chp-check'], (req, res) => {
+  let latestMeter
+  if (req.session.data['gas-added']) {
+    latestMeter = req.session.data.electricitySuppliers[req.session.data.electricitySuppliers.length - 1]
+  } else {
+    latestMeter = req.session.data.gasSuppliers[req.session.data.gasSuppliers.length - 1]
+  }
+  latestMeter.chp.push(req.session.data.chp)
+  switch (req.session.data.chp) {
+    case 'no':
+      res.redirect('/anything-besides-heat-generation')
+      break
+      case 'yes':
+        default:
+          res.redirect('/does-this-meter-supply-the-chp')
+        }
+      })
+      
+router.get(['/chp-supply-check'], (req, res) => {
+  let latestMeter
+  if (req.session.data['gas-added']) {
+    latestMeter = req.session.data.electricitySuppliers[req.session.data.electricitySuppliers.length - 1]
+  } else {
+    latestMeter = req.session.data.gasSuppliers[req.session.data.gasSuppliers.length - 1]
+  }
+  latestMeter.supply.push(req.session.data['chp-supply'])
+  switch (req.session.data['chp-supply']) {
+    case 'no':
+      res.redirect('/anything-besides-heat-generation')
+      break
+    case 'yes':
+    default:
+      res.redirect('/installed-electrical-generation-capacity')
+      break
+  }
+})
+
+router.get(['/egc-check'], (req, res) => {
+  let latestMeter
+  if (req.session.data['gas-added']) {
+    latestMeter = req.session.data.electricitySuppliers[req.session.data.electricitySuppliers.length - 1]
+  } else {
+    latestMeter = req.session.data.gasSuppliers[req.session.data.gasSuppliers.length - 1]
+  }
+  latestMeter.egc.push(req.session.data['egc-chp'])
+  res.redirect('/anything-besides-heat-generation')
+})
+
+router.get(['/besides-check'], (req, res) => {
+  let latestMeter
+  if (req.session.data['gas-added']) {
+    latestMeter = req.session.data.electricitySuppliers[req.session.data.electricitySuppliers.length - 1]
+  } else {
+    latestMeter = req.session.data.gasSuppliers[req.session.data.gasSuppliers.length - 1]
+  }
+  latestMeter.besides.push(req.session.data['besides'])
+  if (req.session.data['gas-added']) {
+    res.redirect('/electricity-supplier-summary')
+  } else {
+    res.redirect('/gas-supplier-summary')
+  }
 })
 
 router.get(['/supplier-check'], (req, res) => {
@@ -113,6 +205,7 @@ router.get(['/supplier-check'], (req, res) => {
       break
     case 'continue-gas':
       if (req.session.data['gas-or-electricity'] === 'both') {
+        req.session.data['gas-added'] = true
         res.redirect('/who-is-your-electricity-supplier')
       } else {
         res.redirect('/supplier-summary')
